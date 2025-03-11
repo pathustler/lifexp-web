563b1a20b979b3d3d557315c6234ba8b0e79c92e
arion52 <bargavkrishna26@gmail.com>
2025-03-11 15:40:44 +0530

    changed sqlite for postgres

--- LifeXP/settings.py
+++ LifeXP/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'LifeXP.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'pattyWantsThisDone20',
+        'HOST': 'db.ratfdpmrikyadkddhaei.supabase.co',  # e.g., db.xxx.supabase.co
+        'PORT': '5432',  # Usually 5432
     }
 }
+
 
 
 # Password validation
