bf099719fb3cdeb5457e7cd6ac821e08d7645925
pathustler <patlowharry@gmail.com>
2025-03-11 21:20:35 +1100

    pull

--- main/views.py
+++ main/views.py
@@ -5,3 +5,8 @@ from . import views
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html')
+
+
+def profile(request, username):
+    
+    return render(request, 'main/profile.html', {'username': username})
