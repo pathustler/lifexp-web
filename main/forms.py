df8c874f2b9c575f02d01061a05feb46874883d7
arion52 <bargavkrishna26@gmail.com>
2025-03-26 11:22:34 +0530

    done with new post

--- main/forms.py
+++ main/forms.py
@@ -7,7 +7,7 @@ class PostForm(forms.ModelForm):
         fields = ['content', 'post_image', 'start_time', 'end_time', "tags"]
         widgets = {
             'content': forms.Textarea(attrs={
-                'class': 'w-full  h-36 px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-300',
+                'class': 'w-full  h-36 px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-300 dark:bg-[#1f2022] dark:border-[#444] dark:text-white dark:focus:ring-blue-600 dark:placeholder-[#444]',
                 'rows': 5,
                 'placeholder': 'Describe your post...'
             }),
@@ -15,20 +15,20 @@ class PostForm(forms.ModelForm):
                 'id': 'image',
                 'name': 'image',
                 'accept': 'image/*',
-                'class': 'absolute inset-0 opacity-0 cursor-pointer',
+                'class': 'absolute inset-0 opacity-0 cursor-pointer placeholder-[#444]',
                 'onchange': 'previewImage(event)',
             }),
             'start_time': forms.TimeInput(attrs={  # Use TimeInput instead of FileInput
                 'type': "time",
                 'id': "start",
                 'name': "start",
-                'class': 'w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-300',
+                'class': 'w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-300 dark:bg-[#1f2022] dark:border-[#444] dark:text-white dark:focus:ring-blue-600 dark:placeholder-[#444]',
             }),
             'end_time': forms.TimeInput(attrs={  # Use TimeInput instead of FileInput
                 'type': "time",
                 'id': "end",
                 'name': "end",
-                'class': 'w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-300',
+                'class': 'w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-300 dark:bg-[#1f2022] dark:border-[#444] dark:text-white dark:focus:ring-blue-600 dark:placeholder-[#444]',
             }),
             'tags': forms.TextInput(attrs={
                 'type': "hidden",
