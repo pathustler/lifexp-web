2cb0d9bd4aaa671de246997e3689eb92df0bccc9
arion52 <bargavkrishna26@gmail.com>
2025-05-14 18:34:00 +0530

    allowed onrender in hosts

--- LifeXP/settings.py
+++ LifeXP/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-6)f*to0rwqdq4u4y+o&xa*p#m+1_!9re!47x8hli#bx(q!+@*a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lifexp-web.onrender.com', 'localhost', '127.0.0.1','onrender.com']
+ALLOWED_HOSTS = ['lifexp-web-zlj3.onrender.com/', 'localhost', '127.0.0.1','onrender.com']
 
 
 # Application definition
